d04b4ec04ac1475d659722cc13bc894744a8d079
A Belgrave <abelgrave95@gmail.com>
2019-03-05 19:27:44 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,8 +1,7 @@
 def alphabetize(arr)
   ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 
-  arr.sort_by { |a, b| ESPERANTO_ALPHABET.index(a) < ESPERANTO_ALPHABET.index(b)}
+  # arr.sort_by { |a, b| ESPERANTO_ALPHABET.index(a) < ESPERANTO_ALPHABET.index(b)}
 
-  Array.new(len,val)
-
+  ESPERANTO_ALPHABET.index(a)
 end
